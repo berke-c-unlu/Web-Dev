2c52e24d621b026912c84efb88ea696df79e45f1
berke-c-unlu <79451411+berke-c-unlu@users.noreply.github.com>
2022-09-02 12:42:59 +0300

    Simon Game has Implemented

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,93 @@
+
+
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).keydown(function(){
+  if(!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function checkAnswer(currentLevel){
+  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    console.log("success");
+
+    if(userClickedPattern.length === gamePattern.length){
+      setTimeout(nextSequence,1000);
+    }
+  }
+  else {
+    wrongAnswerReaction();
+  }
+
+}
+
+function wrongAnswerReaction(){
+  console.log("wrong");
+  var audio = new Audio("sounds/wrong.mp3");
+  audio.play();
+
+  $("body").addClass("game-over");
+
+  setTimeout(function(){
+    $("body").removeClass("game-over");
+  },200);
+
+  $("#level-title").text("Game Over, Press Any Key to Restart");
+
+  started = false;
+  level = 0;
+  userClickedPattern = [];
+  gamePattern = [];
+}
+
+function nextSequence(){
+
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random()*3);
+  var randomChosenColour = buttonColours[randomNumber];
+
+
+
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  },100);
+}
